f22cfaec019451880c95c9a56f3bfbf8d13e5844
Asus <phucanhprono1@gmail.com>
2023-05-12 07:28:56 +0700

    cập nhật giao diện

--- app/src/main/java/com/example/techstore/LocalNetwork.java
+++ app/src/main/java/com/example/techstore/LocalNetwork.java
@@ -1,7 +1,7 @@
 package com.example.techstore;
 
 public class LocalNetwork {
-    private String url="http://192.168.1.29:8888";
+    private String url="https://e204-113-160-14-39.ngrok-free.app";
 
     public String getUrl() {
         return url;
